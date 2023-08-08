76fdde798e3f46369f96fa721e57b1ba48fd2f74
Hooded Paladin <47702210+hoodedpaladin@users.noreply.github.com>
2023-08-08 15:20:31 -0400

    Revert "lastfm_scrobbler check pointers for validity"
    This reverts commit 43abe2d820d6725a9e8a248233539a6ffdc2d712.

--- apps/plugins/lastfm_scrobbler.c
+++ apps/plugins/lastfm_scrobbler.c
@@ -317,11 +317,6 @@ static void scrobbler_flush_callback(void)
 }
 #endif
 
-static inline char* str_chk_valid(char *s, char *alt)
-{
-    return (s != NULL ? s : alt);
-}
-
 static void scrobbler_add_to_cache(const struct mp3entry *id)
 {
     if ( gCache.pos >= SCROBBLER_MAX_CACHE )
@@ -340,19 +335,17 @@ static void scrobbler_add_to_cache(const struct mp3entry *id)
     if (id->tracknum > 0)
         rb->snprintf(tracknum, sizeof (tracknum), "%d", id->tracknum);
 
-    char* artist = id->artist ? id->artist : id->albumartist;
-
     int ret = rb->snprintf(&scrobbler_buf[(SCROBBLER_CACHE_LEN*gCache.pos)],
                        SCROBBLER_CACHE_LEN,
                        "%s\t%s\t%s\t%s\t%d\t%c\t%ld\t%s\n",
-                       str_chk_valid(artist, UNTAGGED),
-                       str_chk_valid(id->album, ""),
-                       str_chk_valid(id->title, ""),
+                       id->artist ? id->artist : id->albumartist,
+                       id->album,
+                       id->title,
                        tracknum,
                        (int)(id->length / 1000),
                        rating,
                        get_timestamp(),
-                       str_chk_valid(id->mb_track_id, ""));
+                       id->mb_track_id);
 
     if ( ret >= SCROBBLER_CACHE_LEN )
     {
