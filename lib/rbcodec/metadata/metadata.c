c8fbd1af99e47dd1e414d07213856204eb4f866f
Hooded Paladin <47702210+hoodedpaladin@users.noreply.github.com>
2023-08-09 00:24:37 -0400

    Move the ITID string too when moving an mp3entry

--- lib/rbcodec/metadata/metadata.c
+++ lib/rbcodec/metadata/metadata.c
@@ -484,6 +484,7 @@ void adjust_mp3entry(struct mp3entry *entry, void *dest, const void *orig)
     MOVE_ENTRY(entry->comment)
     MOVE_ENTRY(entry->albumartist)
     MOVE_ENTRY(entry->grouping)
+    MOVE_ENTRY(entry->itid)
     MOVE_ENTRY(entry->mb_track_id)
 }
 
