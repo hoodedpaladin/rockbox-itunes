f64ca6b5242098fb6b69ca426b56dcac35b218e2
Hooded Paladin <47702210+hoodedpaladin@users.noreply.github.com>
2026-08-04 13:19:58 -0700

    Add option to remove duplicates

--- apps/onplay.c
+++ apps/onplay.c
@@ -572,6 +572,13 @@ static int remove_recent(void)
     return false;
 }
 
+static int remove_duplicates(void)
+{
+    playlist_delete_duplicates(NULL, NULL);
+
+    return false;
+}
+
 MENUITEM_FUNCTION(wps_view_cur_playlist_item, 0, ID2P(LANG_VIEW_DYNAMIC_PLAYLIST),
                   wps_view_cur_playlist, NULL, Icon_NOICON);
 MENUITEM_FUNCTION(search_playlist_item, 0, ID2P(LANG_SEARCH_IN_PLAYLIST),
@@ -588,6 +595,8 @@ MENUITEM_FUNCTION(unqueue_all_item, 0, ID2P(LANG_UNQUEUE_ALL),
                   unqueue_all, NULL, Icon_Playlist);
 MENUITEM_FUNCTION(remove_recent_item, 0, ID2P(LANG_REMOVE_ALL_RECENTLY_PLAYED),
                   remove_recent, NULL, Icon_Playlist);
+MENUITEM_FUNCTION(remove_duplicates_item, 0, ID2P(LANG_REMOVE_DUPLICATES),
+                  remove_duplicates, NULL, Icon_Playlist);
 MAKE_ONPLAYMENU( wps_playlist_menu, ID2P(LANG_CURRENT_PLAYLIST),
                  NULL, Icon_Playlist,
                  &wps_view_cur_playlist_item,
@@ -597,6 +606,7 @@ MAKE_ONPLAYMENU( wps_playlist_menu, ID2P(LANG_CURRENT_PLAYLIST),
                  &playlist_save_item,
                  &reshuffle_item,
                  &remove_recent_item,
+                 &remove_duplicates_item,
                  &playing_time_item
                );
 
